2c8d58b359c34601394b1557f0393effd3c3eac2
PERSEFA1 <epiatrovich@gamil.com>
2024-04-14 01:50:43 +0300

    feat:make components for header and footer

---
+++ front-doc/src/component/footer/FooterComponent.tsx
@@ -0,0 +1,7 @@
+export const FooterComponent=()=>{
+    return(
+        <footer className="Footer-Component">
+            
+        </footer>
+);
+};
---
+++ front-doc/src/component/header/HeaderComponent.tsx
@@ -0,0 +1,18 @@
+export const HeaderComponent=()=>{
+    return(
+        <header className="Header-Component">
+            <a>
+                <img className="Logo" width={400} height={400} src=""></img>
+            </a>
+            <p className="text-header">
+
+            </p>
+            <button className="account">
+
+            </button>
+            <button className="change-of-topic">
+                
+            </button>
+        </header>
+);
+};
