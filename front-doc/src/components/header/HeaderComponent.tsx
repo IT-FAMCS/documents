72703e8736ed4faed1def37baa850299ad90d367
PERSEFA1 <epiatrovich@gamil.com>
2024-04-21 00:43:46 +0300

    fix:button

--- front-doc/src/components/header/HeaderComponent.tsx
+++ front-doc/src/components/header/HeaderComponent.tsx
@@ -1,5 +1,6 @@
 import Stack from "@mui/material/Stack";
 import logo from "../../Images/Raccoon.png";
+import { Button } from "@mui/material";
 
 export const HeaderComponent = () => {
   return (
@@ -15,8 +16,18 @@ export const HeaderComponent = () => {
           <img className="logo" src={logo} alt="Raccoon logo"></img>
         </a>
         <h1 className="header-text">Документооборот Проектного направления</h1>
-        <button className="account">account</button>
-        <button className="change-of-topic">change-of-topic</button>
+        <Button
+          style={{ borderColor: "#000000", color: "#000000" }}
+          variant="outlined"
+        >
+          account
+        </Button>
+        <Button
+          style={{ borderColor: "#000000", color: "#000000" }}
+          variant="outlined"
+        >
+          change of topic
+        </Button>
       </Stack>
     </header>
   );
