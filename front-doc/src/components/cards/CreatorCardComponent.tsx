01af9f3a814206303feeee4ede84e4a3a4d30e92
Simiys <mr.simiys@gmail.com>
2024-04-24 01:43:41 +0300

    feat: created component for cards on Creators page

--- front-doc/src/components/cards/CreatorCardComponent.tsx
+++ front-doc/src/components/cards/CreatorCardComponent.tsx
@@ -1,3 +1,48 @@
-export const CreatorCard = () => {
-    
+import { Card, CardContent, Typography, CardActionArea, Stack } from "@mui/material";
+import "@fontsource/jost";
+import { GitHub, Telegram } from "@mui/icons-material";
+
+interface CreatorCardInterface  {
+    name: "string",
+    surname: "string",
+    role: "string",  
+    telegram: "string", 
+    github: "string"
+}
+
+
+export const CreatorCard: React.FC<CreatorCardInterface> = (props) => {
+
+    const handleTelegramClick = () => {
+        props.telegram
+        window.location.href="www.t.me/" + props.telegram;
+    }
+
+    const handleGithubClick = () => {
+        window.location.href=props.github;
+    }
+
+    return (
+        <Card
+            sx={{
+                width: 300
+            }}
+        >
+            <CardContent 
+                sx={{
+                    alignItems: "center",
+                    textAlign: "center" 
+                }}
+            >
+                <Typography>{props.name} {" "} {props.surname}</Typography>
+                <Typography>{props.role}</Typography>
+                <Stack direction={'row'}>
+                    <Telegram onClick={handleTelegramClick} />
+                    <GitHub onClick={handleGithubClick} />
+                </Stack>
+                
+
+            </CardContent>
+        </Card>
+    )
 }
--- front-doc/src/constants/CreatorsInfo.ts
+++ front-doc/src/constants/CreatorsInfo.ts
@@ -1,27 +1,39 @@
 export const CreatorsInfo = [
-    {
-        name: "Yauheni",
-        surname: "Piatrovich", 
-        telegram: "@epiatrovich",
-        github: "https://github.com/PERSEFA1" 
-     },
-     {
-        name: "Ivan",
-        surname: "Terentiev", 
-        telegram: "@cvrssxxdd",
-        github: "https://github.com/Simiys" 
+   {
+      name: "Yauheni",
+      surname: "Piatrovich", 
+      telegram: "epiatrovich",
+      role: "Fronted",
+      github: "https://github.com/PERSEFA1" 
+   },
+   {
+      name: "Ivan",
+      surname: "Terentiev", 
+      telegram: "cvrssxxdd",
+      role: "Fronted",
+      github: "https://github.com/Simiys" 
      }, 
-     {
-        name: "Evgeniy",
-        surname: "Shevtsov", 
-        telegram: "@pblcb_ev",
-        github: "https://github.com/PblCbGomel" 
-     }, 
-     {
-        name: "Artyom",
-        surname: "Korobkin", 
-        telegram: "@Aiki_v",
-        github: "https://github.com/essaon" 
-     },  
+   {
+      name: "Evgeniy",
+      surname: "Shevtsov", 
+      role: "Fronted/Mentor",
+      telegram: "pblcb_ev",
+      github: "https://github.com/PblCbGomel" 
+   }, 
+   {
+   name: "Artyom",
+      surname: "Korobkin",
+      role: "Backend", 
+      telegram: "Aiki_v",
+      github: "https://github.com/essaon" 
+   },
+   {
+      name: "Atsiom",
+      surname: "Kaktysh",
+      role: "Lead", 
+      telegram: "aaaaaaaaaaaaaaaartem",
+      github: "https://github.com/50ShadesOfCode" 
+   
+   }  
 
 ]
